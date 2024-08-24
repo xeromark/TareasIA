6def6c78aa45c537ff918a681829734ac3ed5c64
Omar Marca <omarjavi03@gmail.com>
2024-08-24 13:18:51 -0400

    a

--- Readme.md
+++ Readme.md
@@ -2,9 +2,29 @@
 
     pip install bnlearn
 
-
-
 # Se importan las librerías necesarias:
 
     import pandas as pd
     import bnlearn as bn
+
+# Se usa el siguiente código:
+
+Exhaustivesearch determina todas las posibles estructuras de red bayesiana en un conjunto de datos para seleccionar la estructura que se adecue mejor estos. Por lo tanto, este método garantiza encontrar la estructura más óptima a costa de usar muchos recursos computacionales de la máquina en donde se utiliza.
+
+```
+
+import bnlearn as bn
+from tabulate import tabulate
+import pandas as pd
+
+df = pd.read_csv('dataset.csv')
+
+model = bn.structure_learning.fit(df, methodtype='ex', scoretype='bic')
+
+model = bn.independence_test(model, df, alpha=0.05, prune=True)
+
+print(tabulate(model['independence_test'], tablefmt="grid", headers="keys"))
+bn.plot(model)
+
+ ```
+
--- tarea.py
+++ tarea.py
@@ -1,11 +1,26 @@
-import pandas as pd
+# Load library
 import bnlearn as bn
+from tabulate import tabulate
+import pandas as pd
 
-# Cargar el archivo CSV
-df = pd.read_csv('./dataset.csv')
+# Load example
+df = pd.read_csv('dataset.csv')
 
 
-model = bn.structure_learning.fit(df)
+# Structure learning
+model = bn.structure_learning.fit(df, methodtype='ex', scoretype='bic')
+# Compute edge strength using chi-square independence test and remove (prune) the not-signficant edges
 
-print(model['adjmat'])
-plot = bn.plot(model)
+model = bn.independence_test(model, df, alpha=0.05, prune=True)
+
+# Examine the output of the chi-square test. All P values are significant. Nothing is removed.
+print(tabulate(model['independence_test'], tablefmt="grid", headers="keys"))
+#    source     target     stat_test        p_value    chi_square    dof
+#--  ---------  ---------  -----------  -----------  ------------  -----
+# 0  Cloudy     Rain       True         1.08061e-87       394.062      1
+# 1  Cloudy     Sprinkler  True         8.38371e-53       233.906      1
+# 2  Rain       Wet_Grass  True         3.88651e-64       285.902      1
+# 3  Sprinkler  Wet_Grass  True         1.19692e-23       100.478      1
+
+# Plot
+bn.plot(model)
