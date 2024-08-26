2abae2b2af5e5be2cb1268b942162393a65e95c1
Omar Marca <omarjavi03@gmail.com>
2024-08-26 10:48:33 -0400

    a

--- p2.py
+++ p2.py
@@ -1,6 +1,7 @@
+import numpy as np
+from tabulate import tabulate
 
-
-matriz = [
+Matriz = np.array([
     [0.25, 0.06, 0.08, 0.15, 0.04, 0.02, 0.15, 0.15, 0.10],  # G1
     [0.15, 0.15, 0.10, 0.22, 0.01, 0.02, 0.15, 0.10, 0.10],  # G2
     [0.12, 0.00, 0.05, 0.24, 0.14, 0.04, 0.27, 0.07, 0.07],  # G3
@@ -10,13 +11,18 @@ matriz = [
     [0.01, 0.05, 0.15, 0.14, 0.17, 0.10, 0.12, 0.10, 0.16],  # G7
     [0.17, 0.15, 0.07, 0.07, 0.15, 0.10, 0.12, 0.09, 0.08],  # G8
     [0.13, 0.11, 0.13, 0.03, 0.20, 0.20, 0.04, 0.15, 0.01]   # G9
-]
+])
 
 
 estados = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 estadoActual = [0, 0, 0, 0, 1, 0, 0, 0, 0]
 temporal = [] #lista vacia para guardar los datos.
 
+for x in range(10):
+   estadoActual =np.dot(estadoActual , Matriz)
+   temporal.append(estadoActual)
+
+temporal=np.array(temporal)
 
 
-
+print(tabulate(temporal))
