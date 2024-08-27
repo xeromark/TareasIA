81935f1f009434050a409f8f2500c4cb134cfb36
Omar Marca <omarjavi03@gmail.com>
2024-08-26 22:38:36 -0400

    p2

--- p2.py
+++ p2.py
@@ -1,7 +1,9 @@
 import numpy as np
 from tabulate import tabulate
+import matplotlib.pyplot as plt
 
 Matriz = np.array([
+   #  G1    G2    G3    G4    G5    G6    G7    G8    G9
     [0.25, 0.06, 0.08, 0.15, 0.04, 0.02, 0.15, 0.15, 0.10],  # G1
     [0.15, 0.15, 0.10, 0.22, 0.01, 0.02, 0.15, 0.10, 0.10],  # G2
     [0.12, 0.00, 0.05, 0.24, 0.14, 0.04, 0.27, 0.07, 0.07],  # G3
@@ -19,10 +21,28 @@ estadoActual = [0, 0, 0, 0, 1, 0, 0, 0, 0]
 temporal = [] #lista vacia para guardar los datos.
 
 for x in range(10):
-   estadoActual =np.dot(estadoActual , Matriz)
-   temporal.append(estadoActual)
+   estadoActual = np.dot(estadoActual , Matriz) # Acumulacion del estado actual con los siguientes
+   temporal.append(estadoActual) # Cada iteracion se alamcena su resultado acumulado
+
 
 temporal=np.array(temporal)
 
+aux = []
 
-print(tabulate(temporal))
+for i in [1,3,5,7,9]:
+  indice_temporal = estados.index(i)
+
+  plt.plot(temporal[: , indice_temporal], label=f'Canción G{i}')
+
+
+print(tabulate(temporal[:, [0,2,4,6,8]], headers=["G1","G3","G5","G7","G9"], tablefmt="grid")) # Mostrar la tabla tabulada asociada al grafico 
+
+
+
+
+plt.xlabel('Iteración')
+plt.ylabel('Probabilidad (%)')
+plt.title('Probabilidad de reproducción de música')
+plt.legend()
+plt.grid(True)
+plt.show()
