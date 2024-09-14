7e5ebc2b46f94021f1d935f093d3dd61c3c0cbde
Omar Marca <omarjavi03@gmail.com>
2024-09-14 00:15:08 -0300

    a

--- Hillclimbsearch.py
+++ Hillclimbsearch.py
@@ -3,18 +3,13 @@ import bnlearn as bn
 import pandas as pd
 from tabulate import tabulate
 
-# Load example
 df = pd.read_csv('dataset.csv')
 
-# Structure learning
 modelhc = bn.structure_learning.fit(df, methodtype='hc', scoretype='bic')
-# Plot detected DAG
 
-# Compute edge strength using chi-square independence test
 modelhc = bn.independence_test(modelhc, df, alpha=0.05, prune=False)
 bn.plot(modelhc)
 
-# Examine the output of the chi-square test. 53 edges are detected but not all P values are significant, i.e. those with stat_test=False
 print(tabulate(modelhc['independence_test'], headers="keys"))
 
 
