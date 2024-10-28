1c1d7b02de8405e5e6275b988b4d211468965fe9
Omar Marca <omarjavi03@gmail.com>
2024-10-27 23:30:34 -0300

    a

--- p1.py
+++ p1.py
@@ -1,47 +1,27 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, recall_score, confusion_matrix
+from sklearn import metrics
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv('dataset.csv') # https://www.kaggle.com/datasets/kandij/diabetes-dataset
+df = pd.read_csv('dataset.csv') # https://www.kaggle.com/datasets/alexteboul/heart-disease-health-indicators-dataset
 
-# Seleccionar la característica del dataset de BMI y Outcome
+# Seleccionar la característica del dataset de BMI y HighBP
 x = df[['BMI']]    # Indice de masa corporal (BMI)
-y = df['Outcome']  # si tiene o no diabetes (Outcome)
+y = df['HighBP']  # si tiene o no Hipertensión (HighBP)
 
 # Dividir el conjunto de datos en entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.5, random_state=1)
 # el 50% de los datos se usan para pruebas y el resto para entrenar (parametro asociado a test_size)
 
-model = LogisticRegression() # Crear el modelo de regresión logística
+model = LogisticRegression(max_iter=100000) # Crear el modelo de regresión logística
 model.fit(X_train, y_train) # entrenar el modelo
 
 # Predecir en el conjunto de prueba
 predictions = model.predict(X_test)
-
-print("Donde:")
-
-cm = confusion_matrix(y_test, predictions)
-# Se extraen TP, FP, FN y TN
-TN, FP, FN, TP = cm.ravel()  # Desempaquetar los valores
-print(f'TP (Verdaderos Positivos): {TP}')
-print(f'FP (Falsos Positivos): {FP}')
-print(f'FN (Falsos Negativos): {FN}')
-print(f'TN (Verdaderos Negativos): {TN} \n')
-
-print("A partir de esto, se obtienen las siguiente métricas:")
-
-
-# Se calculan las métricas
-accuracy = accuracy_score(y_test, predictions)
-print(f'Acuraccy: {accuracy:.2f}')
-
-recall = recall_score(y_test, predictions)
-print(f'Recall: {recall:.2f}\n')
-
 
 print("Luego se tiene el siguiente gráfico:")
 
@@ -63,10 +43,37 @@ plt.plot(BMI_range, G_z, color='red', label='Frontera de decisión')
 
 
 # Configuración del gráfico
-plt.title('Clasificación de Diabetes basada en el BMI')
+plt.title('Clasificación de Hipertensión basada en el BMI')
 plt.xlabel('Índice de Masa Corporal (BMI)')
-plt.ylabel('Resultado de Diabetes (0 = No, 1 = Sí)')
+plt.ylabel('Resultado de Hipertensión (0 = No, 1 = Sí)')
 plt.yticks([0, 1])
 plt.legend()
 plt.grid()
 plt.show()
+
+
+# Para mostrar el gráfico de la matriz de confusión
+cm = confusion_matrix(y_test, predictions)
+cm_display = metrics.ConfusionMatrixDisplay(cm, display_labels = [False, True])
+cm_display.plot(cmap='plasma')
+plt.title('Logistic Regression')
+plt.show()
+
+print("Donde:")
+
+# Se extraen TP, FP, FN y TN
+TN, FP, FN, TP = cm.ravel()  # Desempaquetar los valores
+print(f'TP (Verdaderos Positivos): {TP}')
+print(f'FP (Falsos Positivos): {FP}')
+print(f'FN (Falsos Negativos): {FN}')
+print(f'TN (Verdaderos Negativos): {TN} \n')
+
+print("A partir de esto, se obtienen las siguiente métricas:")
+
+
+# Se calculan las métricas
+accuracy = accuracy_score(y_test, predictions)
+print(f'Acuraccy: {accuracy:.2f}')
+
+recall = recall_score(y_test, predictions)
+print(f'Recall: {recall:.2f}\n')
